82e4dccbb723ef8c166b8d3d26dc46fa72991b57
나은 <kkne1017@gmail.com>
2024-04-30 23:50:12 +0900

    Create 1075.py

---
+++ 1075.py
@@ -0,0 +1,20 @@
+
+print("N입력:")
+N= int(input())
+print("P입력:")
+F= int(input())
+
+num1 = N // 100
+num2 = num1 * 100
+while num2% F != 0:
+    num2 +=1
+            
+
+
+
+
+num2 = num2 % 100
+if num2 < 10:
+    print(f"0{num2}")
+else: 
+    print(num2)
