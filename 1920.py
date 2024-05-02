81d5f73d502e71dc55dbd0ca7844dfa74a248702
김나은 <145482647+betteree@users.noreply.github.com>
2024-05-02 17:46:50 +0900

    Update 1920.py

--- 1920.py
+++ 1920.py
@@ -13,12 +13,12 @@ while i < len(B):
     found = False
 
     while low <= high:
-        mid = (low + high) // 2  # 중간 인덱스 계산
+        mid = (low + high) // 2
 
         if A[mid] < B[i]:
-            low = mid + 1  # 중간 값보다 크므로 오른쪽 부분 탐색
+            low = mid + 1  
         elif A[mid] > B[i]:
-            high = mid - 1  # 중간 값보다 작으므로 왼쪽 부분 탐색
+            high = mid - 1  
         else:
             found = True
             break
